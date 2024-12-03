a31223882ac5025e188413d490121c42f89dd91a
Dennis-83 <dodo_ez@hotmail.com>
2024-12-03 22:48:40 +0100

    Added fetchCountries function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import './App.css';
+import axios from "axios";
+import {useState} from "react";
 
 function App() {
 
+    const [countries, setCountries] = useState([]);
+
+    async function fetchCountries() {
+        try {
+            const result = await axios.get(`https://restcountries.com/v3.1/all?fields=name,flag,population,region`);
+            setCountries(result.data);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     return (
         <>
-            Maak je applicatie hier!
-            Hello World!
+            <button type="button" onClick={fetchCountries}
+            >Haal landen op!
+            </button>
+            {/*{console.log(countries)}*/}
+
+            {countries.map((country) => {
+                // console.log(country.name.common);
+                return <>
+                <p>
+                    {country.flag} {country.name.common}
+                </p>
+                <p>
+                    Has a population of {country.population}
+                </p>
+                <p>
+                    {country.region}
+                </p>
+                </>
+
+            })}
+            {/*{countries[0] && <p>{countries[0].name.common}</p>}*/}
         </>
     )
 }
