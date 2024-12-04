b5b563b012f1853cab4e5eb6486fc04be8b98826
Dennis-83 <dodo_ez@hotmail.com>
2024-12-04 13:01:31 +0100

    Button now disappears after succesfully fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,13 @@ function App() {
     async function fetchCountries() {
         try {
             const result = await axios.get(`https://restcountries.com/v3.1/all?fields=name,flag,population,region`);
-            setCountries(result.data.sort((countryA, countryB) => {
-                return countryA.population - countryB.population
-            }));
+            setCountries(result.data.sort((countryA, countryB) => countryA.population - countryB.population));
         } catch (e) {
             console.error(e);
         }
     }
 
-    function changeCountryTextColor(country){
+    function changeCountryTextColor(country) {
         let color = '';
         switch (country.region) {
             case 'Africa':
@@ -41,27 +39,25 @@ function App() {
 
     return (
         <>
-            <button type="button" onClick={fetchCountries}
+            {!countries[0] && <button type="button" onClick={fetchCountries}
             >Haal landen op!
-            </button>
-            {/*{console.log(countries)}*/}
+            </button>}
+            {/*{console.log(!countries[0])}*/}
 
             {countries.map((country) => {
                 // console.log(country.name.common);
                 return <>
-                <p className={changeCountryTextColor(country)}>
-                    {country.flag} {country.name.common}
-                </p>
-                <p>
-                    Has a population of {country.population}
-                </p>
-                <p>
-                    {country.region}
-                </p>
+                    <p className={changeCountryTextColor(country)}>
+                        {country.flag} {country.name.common}
+                    </p>
+                    <p>
+                        Has a population of {country.population}
+                    </p>
+                    <p>
+                        {country.region}
+                    </p>
                 </>
-
             })}
-            {/*{countries[0] && <p>{countries[0].name.common}</p>}*/}
         </>
     )
 }
